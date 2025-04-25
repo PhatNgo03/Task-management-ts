058205be5b3132d24fadc56b5d11117b12e9d94b
PhatNgo03 <phatngo1908@gmail.com>
2025-04-25 17:18:05 +0700

    create  routes Task

---
+++ api/v1/routes/task.route.ts
@@ -0,0 +1,25 @@
+import {Router, Request, Response} from "express";
+const router: Router = Router();
+
+import Task from "../../../models/task.model";
+
+router.get("/",async (req: Request, res: Response) => {
+  const tasks = await Task.find({
+    deleted: false,
+
+  });
+  res.json(tasks);
+});
+
+router.get("/detail/:id",async (req: Request, res: Response) => {
+  const id:string  = req.params.id;
+
+  const tasks = await Task.find({
+    _id: id,
+    deleted: false,
+
+  });
+  res.json(tasks);
+});
+
+export const taskRoutes: Router = router;
