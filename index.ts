bef4b9427f4b30eb823d9e923aaaca4b03351470
PhatNgo03 <phatngo1908@gmail.com>
2025-05-03 13:21:51 +0700

    fix pug

--- index.ts
+++ index.ts
@@ -26,8 +26,42 @@ app.use(cors());
 //   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 // }
 // app.use(cors(corsOptions));
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
 
 
+
+
+// DEPLOY project vercel
+// import express, { Express } from "express";
+// import dotenv from "dotenv";
+// import * as database from "./config/database";
+// import cors from "cors";
+// import mainV1Routes from "./api/v1/routes/index.route";
+// dotenv.config();
+// database.connect();
+
+
+// const app: Express = express();
+// const port: number | string = process.env.PORT || 3000;
+
+// // c1: Parse application/json
+// // app.use(bodyParser.json());
+// //c2
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// mainV1Routes(app);
+
+// //CORS
+// app.use(cors());
+ 
+// import serverless from "serverless-http";
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// mainV1Routes(app);
+
+// export const handler = serverless(app);
